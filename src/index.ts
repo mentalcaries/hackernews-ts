ba3e820f4a8e308631bdd68afb174a6b9b330460
Devin Jaggernauth <neexis@gmail.com>
2022-09-06 12:54:56 -0400

    create Link

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { ApolloServer } from 'apollo-server';
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core"
+
+import { schema } from './schema';
+
+export const server = new ApolloServer({
+  schema,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground]
+})
+
+const PORT = 3000;
+
+server.listen(PORT)
+.then(({ url })=> {
+  console.log(`🚀 Server ready at ${url}`)
+})
